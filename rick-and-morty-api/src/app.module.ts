9f9f92e44b9b5a2026b2a3de76de6af6e758bc6d
Felipe Defendi Prado <84990172+felipedefendi@users.noreply.github.com>
2024-06-24 14:12:13 -0300

    Update app.module.ts

--- rick-and-morty-api/src/app.module.ts
+++ rick-and-morty-api/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RickAndMortyModule } from './rick-and-morty/rick-and-morty.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/user.module';
 import { LogsModule } from './logs/log.module';
 import { CommonModule } from './common/common.module';
 
